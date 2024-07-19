798b737e577185549ea5d3b2883b4e3da6ccf05e
Kauê Lima <kaue5939.lima@gmail.com>
2024-07-19 12:44:08 -0300

    first commit

---
+++ exercicio1JSBasico/index.js
@@ -0,0 +1,17 @@
+// Objetivo: apresente a mensagem, "Seja bem vindo(a),
+//Fulano!"
+
+/* 
+1 - Peça que o usuário informe o seu nome usando o prompt
+(lembre-se da aula de variáveis);
+
+2 - Apresente a mensagem de boas vindas seguindo do
+nome que a pessoa digitou no passo 1.
+
+Obs: pra apresentar a mensagem de boas vindas pode usar
+o alert também.
+*/
+
+const nomeDoUsuario = prompt('Digite seu nome:')
+
+alert(`Seja bem-vindo(a), ao curso de Javascript ${nomeUsuario}`)
